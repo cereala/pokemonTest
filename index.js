58157e7a8f0a0cc1f9efeb43d0960498cba27bfa
Sergiu Bodea <nesquick.killer@gmail.com>
2020-05-29 18:48:21 +0300

    'Second'

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import morgan from 'morgan'
-import config from './config/config.js'
-import pokemons from './routes/api/pokemons.js'
-import multer from 'multer'
-import uploadImage from './helpers/helpers.js'
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import config from "./config/config.js";
+import pokemons from "./routes/api/pokemons.js";
+import multer from "multer";
+import uploadImage from "./helpers/helpers.js";
 
-const app = express()
+const app = express();
 const multerMid = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-        fileSize: 5 * 1024 * 1024 
-    }
-})
-
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 //Middleware
-app.use(multerMid.single('pokemonImage'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(morgan('dev'))
+app.use(multerMid.single("pokemonImage"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
 mongoose
-    .connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB ATLAS connected'))
-    .catch(err => console.log(err))
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB ATLAS connected"))
+  .catch((err) => console.log(err));
 
-app.use('/api/pokemons', pokemons)
+app.use("/api/pokemons", pokemons);
 
-app.use('/', (req, res, next) =>
-    res.status(200).json({message: `This works for mainpage! at path ${req.path}`})
-)
+app.use("/", (req, res, next) => {
+  res
+    .status(200)
+    .json({ message: `This works for mainpage! at path ${req.path}` });
+  next();
+});
 
 app.use((req, res, next) => {
-    const error = new Error(`Path ${req.path} not found. Please use /api/pokemons and refer to the documentation.`)
-    error.status = 404
-    next(error)
-})
+  const error = new Error(
+    `Path ${req.path} not found. Please use /api/pokemons and refer to the documentation.`
+  );
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-       .json({
-           error: {message: error.message}
-       })
-})
+  res.status(error.status || 500).json({
+    error: { message: error.message },
+  });
+});
 
 app.listen(config.PORT, () => {
-    console.log('Server running on port: ' + config.PORT)
-})
+  console.log("Server running on port: " + config.PORT);
+});
