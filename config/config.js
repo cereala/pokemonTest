c19e4fc773147c1dac26953ccb5aced885a59395
Sergiu Bodea <nesquick.killer@gmail.com>
2020-05-29 19:24:56 +0300

    env var

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 5000,
     URL: process.env.BASE_URL || 'http://localhost:5000',
-    MONGODB_URI: process.env.NODE_ENV || 'mongodb+srv://sergiuBodea77:mxck2577@myfirstcluster-6bvkx.mongodb.net/test?retryWrites=true&w=majority'
+    MONGODB_URI: 'mongodb+srv://sergiuBodea77:mxck2577@myfirstcluster-6bvkx.mongodb.net/test?retryWrites=true&w=majority'
 } 
 
 export default config
